68b81a60273c24fd50804f7b0d981d2de18b2d9f
Ro1dex <sanhcez58@gmail.com>
2023-10-11 12:15:21 +0200

    complete 1 task, 2.function

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,6 +1,26 @@
 function getArrayParams(...arr) {
 
-  return { min: min, max: max, avg: avg };
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+  }
+
+  const avg = (sum / arr.length).toFixed(2);
+
+  return {
+    min: Number(min),
+    max: Number(max),
+    avg: Number(avg)
+  };
 }
 
 function summElementsWorker(...arr) {
