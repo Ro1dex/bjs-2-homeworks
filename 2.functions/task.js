c53c6109853fba65e4a5ff150b74f3d4fcddcd80
Ro1dex <sanhcez58@gmail.com>
2023-10-11 12:23:35 +0200

    completes 2 task, 2.function

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -24,6 +24,11 @@ function getArrayParams(...arr) {
 }
 
 function summElementsWorker(...arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+  sum += arr[i];
+  }
+  return sum;
 
 }
 
