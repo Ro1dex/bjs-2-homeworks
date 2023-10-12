7e89d0361612bf2393c59f2ba1cffc2c92a514bb
Ro1dex <sanhcez58@gmail.com>
2023-10-12 20:22:58 +0200

    edited arrays/task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,6 @@
-function compareArrays(arr1, arr2) {
-  
-}
+const compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
 
 function getUsersNamesInAgeRange(users, gender) {
-  
-}
+    const filteredUsers = users.filter(user => user.gender === gender);
+    return filteredUsers.length ? filteredUsers.reduce((acc, user) => acc + user.age, 0) / filteredUsers.length : 0;
+} 
